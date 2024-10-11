7133671acc2483efff14e4a1d1bae5afd39735e6
MishaPanivnyk <panivnykm@gmail.com>
2024-10-11 23:36:00 +0300

    add components/App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,20 @@
-import './App.css';
+import React from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { Routes, Route } from 'react-router-dom';
+import SharedLayout from 'components/SharedLayout/SharedLayout';
+import MainPage from 'pages/MainPage/MainPage';
 
-function App() {
+const App: React.FC = () => {
   return (
     <>
-      <h1>❤️Misha Panivnyk + Bodya Bayurchak❤️</h1>
+      <GlobalStyle />
+      <SharedLayout>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </SharedLayout>
     </>
   );
-}
+};
 
 export default App;
