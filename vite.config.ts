20ff1946c8416efc3071bef2d89adbce12d4e3af
MishaPanivnyk <panivnykm@gmail.com>
2024-10-11 23:34:33 +0300

    add config/path-resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: 'cryonix-frontend',
-});
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      // Додайте інші алиаси, якщо потрібно
+    },
+  }, // Закриваюча дужка для resolve
+}); // Закриваюча дужка для defineConfig
