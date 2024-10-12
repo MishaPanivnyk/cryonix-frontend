ae1305fe36bb2af09e70e2182afd14210078c934
MishaPanivnyk <panivnykm@gmail.com>
2024-10-12 22:12:11 +0300

    Update Container.styled.ts

--- src/components/Container/Container.styled.ts
+++ src/components/Container/Container.styled.ts
@@ -6,6 +6,9 @@ export const Wrapper = styled.div`
   padding-left: 10px;
   padding-right: 10px;
   min-width: 320px;
+  @media screen and (max-width: 767px) {
+    max-width: 320px;
+  }
   @media only screen and (min-width: 768px) {
     padding-left: 50px;
     padding-right: 50px;
